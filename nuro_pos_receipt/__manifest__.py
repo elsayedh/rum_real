4293c7b652ecf1e7b859f356de8016b3138f7fe9
Mohammed Alwasefy <mohammed.alwasefy@gmail.com>
2020-12-08 17:52:24 +0200

    Add files via upload

---
+++ nuro_pos_receipt/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS New Receipt',
+    'version': '13.0.1.0.0',
+    'summary': """Pos Custom Receipt change odoo POS receipt in table format""",
+    'description': """
+                    Change Odoo POS Receipt
+                    Odoo POS Receipt table format
+                    New Odoo POS Receipt                                      
+                    """,
+    'author': "Nurosolution pvt Ltd",
+    'category': 'Sales/Point Of Sale',
+    'website': 'nurosolution.com',
+    'support': 'info@nurosolution.com',
+    'company': 'Nurosolution Pvt Ltd',
+    'license': 'OPL-1',
+    'images': ['static/description/banner.png'],
+    'depends': ['base', 'point_of_sale'],
+    'data': ['assets/assets.xml'],
+    'demo': [],
+    'qweb': ['static/src/xml/pos.xml'],
+    'installable': True,
+}
